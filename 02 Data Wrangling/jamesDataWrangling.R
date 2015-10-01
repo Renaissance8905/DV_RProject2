b463285b9959c77db054d98ace74c9a76972792e
unknown <james_h.thorpe@yahoo.com>
2015-10-01 16:19:18 -0500

    Updates: Graphics, DevTracking,

--- 02 Data Wrangling/jamesDataWrangling.R
+++ 02 Data Wrangling/jamesDataWrangling.R
@@ -1,5 +1,8 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
 
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cjs2599', PASS='orcl_cjs2599', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-
+View(df1)
 
