5d94ecf4167d8972d243162336f1af97d2d5bd39
unknown <james_h.thorpe@yahoo.com>
2015-10-02 14:01:10 -0500

    Upadtes to my datavis

--- 02 Data Wrangling/jamesDataWrangling.R
+++ 02 Data Wrangling/jamesDataWrangling.R
@@ -6,9 +6,9 @@ dfjames <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native
 
 dfjames2 <-data.frame(dfjames)
 
-View(dfjames2)
+dfjames2 %>% filter(CATEGORY_ETXT %in% c("Car") | grepl("Truck", CATEGORY_ETXT)) %>% View
 
-dfjames2 %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, UNIT_AFFECTED_NBR) %>% filter(CATEGORY_ETXT %in% c("Car", "Motorcycle", "Truck - Med. and H.D.", "Light Truck and Van"),  SYSTEM_TYPE_ETXT != "Not Entered", YEAR != -9999, SYSTEM_TYPE_ETXT %in% c("Brakes", "Structure", "Engine", "Airbag")) %>% arrange(YEAR) %>% ggplot(aes(x = as.character(SYSTEM_TYPE_ETXT), y = as.numeric(as.character(YEAR)), color = MAKE_NAME_NM)) + facet_grid(.~CATEGORY_ETXT) + geom_point()
+dfjames2 %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, UNIT_AFFECTED_NBR, COMMENT_ETXT) %>% filter(CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV", "Bus") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999, SYSTEM_TYPE_ETXT %in% c("Brakes", "Structure", "Engine", "Airbag") | grepl("Brakes", COMMENT_ETXT) | grepl("Airbag", COMMENT_ETXT) | grepl("Structure", COMMENT_ETXT) | grepl("Engine", COMMENT_ETXT) ) %>% arrange(YEAR) %>% ggplot(aes(x = as.character(SYSTEM_TYPE_ETXT), y = as.numeric(as.character(YEAR)), color = MAKE_NAME_NM)) + facet_grid(.~CATEGORY_ETXT) + geom_point()
   
   
   ggplot () +
