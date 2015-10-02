d08e343cb3e6bf22399c2bff2246196881783371
Quan Nguyen <quannguyen1604@gmail.com>
2015-10-02 16:43:38 -0500

    updated my visualization with description and comment

--- 02 Data Wrangling/quanDataWrangling.R
+++ 02 Data Wrangling/quanDataWrangling.R
@@ -4,8 +4,23 @@ require(ggplot2)
 require("jsonlite")
 require("RCurl")
 
-dfquan <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_qan74', PASS='orcl_qan74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_qan74', PASS='orcl_qan74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-View(dfquan)
+quandf <- df %>% select(MAKE_NAME_NM, SYSTEM_TYPE_ETXT, UNIT_AFFECTED_NBR, YEAR) %>% group_by(YEAR) %>% filter(MAKE_NAME_NM %in% c("HONDA", "ACURA"), SYSTEM_TYPE_ETXT != "Accessories", SYSTEM_TYPE_ETXT != "Label")
 
-dfquan %>% select(MAKE_NAME_NM, MODEL_NAME_NM, SYSTEM_TYPE_ETXT, UNIT_AFFECTED_NBR, YEAR) %>% group_by(MODEL_NAME_NM) %>% filter(MAKE_NAME_NM %in% c("HONDA", "ACURA"), SYSTEM_TYPE_ETXT != "Not Entered", SYSTEM_TYPE_ETXT != "Other", SYSTEM_TYPE_ETXT != "Accessories") %>% ggplot(aes(x = as.numeric(as.character(YEAR)), y = as.numeric(as.character(UNIT_AFFECTED_NBR)), color = SYSTEM_TYPE_ETXT)) + geom_point()
+View(quandf)
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='Honda/Acura Recalls') +
+  labs(x="YEAR", y="UNITS AFFECTED") +
+  layer(data=quandf, 
+        mapping=aes(x= as.numeric(as.character(YEAR)), y=as.numeric(as.character(UNIT_AFFECTED_NBR)), color=SYSTEM_TYPE_ETXT), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_jitter(width=0.3, height=0)
+)
