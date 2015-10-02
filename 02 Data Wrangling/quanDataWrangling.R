bc97aa7c3b5dfe54020cf49b2337617d12048f48
Quan Nguyen <quannguyen1604@gmail.com>
2015-10-02 11:43:06 -0500

    added my plot about honda

--- 02 Data Wrangling/quanDataWrangling.R
+++ 02 Data Wrangling/quanDataWrangling.R
@@ -4,6 +4,8 @@ require(ggplot2)
 require("jsonlite")
 require("RCurl")
 
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_qan74', PASS='orcl_qan74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+dfquan <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_qan74', PASS='orcl_qan74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-View(df)
+View(dfquan)
+
+dfquan %>% select(MAKE_NAME_NM, MODEL_NAME_NM, SYSTEM_TYPE_ETXT, UNIT_AFFECTED_NBR, YEAR) %>% group_by(MODEL_NAME_NM) %>% filter(MAKE_NAME_NM == "HONDA", SYSTEM_TYPE_ETXT != "Not Entered", SYSTEM_TYPE_ETXT != "Other") %>% ggplot(aes(x = as.numeric(as.character(YEAR)), y = as.numeric(as.character(UNIT_AFFECTED_NBR)), color = SYSTEM_TYPE_ETXT)) + geom_point()
