5d0f63500adbccda59ff85a6186625be402e82c9
Quan Nguyen <quannguyen1604@gmail.com>
2015-10-02 13:55:35 -0500

    added summary & subset; changed my plot slightly

--- 02 Data Wrangling/quanDataWrangling.R
+++ 02 Data Wrangling/quanDataWrangling.R
@@ -8,4 +8,4 @@ dfquan <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/
 
 View(dfquan)
 
-dfquan %>% select(MAKE_NAME_NM, MODEL_NAME_NM, SYSTEM_TYPE_ETXT, UNIT_AFFECTED_NBR, YEAR) %>% group_by(MODEL_NAME_NM) %>% filter(MAKE_NAME_NM == "HONDA", SYSTEM_TYPE_ETXT != "Not Entered", SYSTEM_TYPE_ETXT != "Other") %>% ggplot(aes(x = as.numeric(as.character(YEAR)), y = as.numeric(as.character(UNIT_AFFECTED_NBR)), color = SYSTEM_TYPE_ETXT)) + geom_point()
+dfquan %>% select(MAKE_NAME_NM, MODEL_NAME_NM, SYSTEM_TYPE_ETXT, UNIT_AFFECTED_NBR, YEAR) %>% group_by(MODEL_NAME_NM) %>% filter(MAKE_NAME_NM %in% c("HONDA", "ACURA"), SYSTEM_TYPE_ETXT != "Not Entered", SYSTEM_TYPE_ETXT != "Other", SYSTEM_TYPE_ETXT != "Accessories") %>% ggplot(aes(x = as.numeric(as.character(YEAR)), y = as.numeric(as.character(UNIT_AFFECTED_NBR)), color = SYSTEM_TYPE_ETXT)) + geom_point()
