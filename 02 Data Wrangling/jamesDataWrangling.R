4f3552b7a5b409daacfa8548cc9db34491818bd6
unknown <james_h.thorpe@yahoo.com>
2015-10-02 18:45:23 -0500

    no message

--- 02 Data Wrangling/jamesDataWrangling.R
+++ 02 Data Wrangling/jamesDataWrangling.R
@@ -4,22 +4,30 @@ require(ggplot2)
 
 dfjames <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from recalls;"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jht585', PASS='orcl_jht585', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter(CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999, SYSTEM_TYPE_ETXT %in% c("Brakes", "Structure", "Engine", "Airbag") | grepl("Brake", COMMENT_ETXT) | grepl("BRAKE", COMMENT_ETXT) | grepl("brake", COMMENT_ETXT) | grepl("Airbag", COMMENT_ETXT) | grepl("Structure", COMMENT_ETXT) | grepl("Engine", COMMENT_ETXT) ) %>% arrange(YEAR) %>% ggplot(aes(x = as.character(SYSTEM_TYPE_ETXT), y = as.numeric(as.character(YEAR)), color = MAKE_NAME_NM)) + facet_grid(.~CATEGORY_ETXT) + geom_point() + theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5))
+
+#create new datasets for each particular system failure to study. Use grepl to search for keywords in the comments col, and mutate to make new col that combines info from comments and system col. 
+
+dfbrake <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999, SYSTEM_TYPE_ETXT == "Brake" | grepl("Brake", COMMENT_ETXT) | grepl("BRAKE", COMMENT_ETXT) | grepl("brake", COMMENT_ETXT) ) %>% mutate (SYS_FAIL = "brake"))
+
+dfairbag <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT), YEAR != -9999, SYSTEM_TYPE_ETXT == "airbag" | grepl("airbag", COMMENT_ETXT) | grepl("air bag", COMMENT_ETXT) | grepl("AIRBAG", COMMENT_ETXT) | grepl("AIR BAG", COMMENT_ETXT) ) %>% mutate (SYS_FAIL = "airbag"))
+
+dfstructure <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999, SYSTEM_TYPE_ETXT == "Structure", SYSTEM_TYPE_ETXT != "Brake", SYSTEM_TYPE_ETXT != "Engine", SYSTEM_TYPE_ETXT != "Airbag"| grepl("fracture", COMMENT_ETXT) | grepl("weld", COMMENT_ETXT) | grepl("crack", COMMENT_ETXT) | grepl("CRACK", COMMENT_ETXT) | grepl("CLIPS", COMMENT_ETXT) | grepl("hood", COMMENT_ETXT) | grepl("HOOD", COMMENT_ETXT) | grepl("pin", COMMENT_ETXT) | grepl("PIN", COMMENT_ETXT) | grepl("frame", COMMENT_ETXT) | grepl("mount", COMMENT_ETXT) ) %>% mutate(SYS_FAIL = "structure"))
 
 
- 
-dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT == "Car", YEAR != -9999) %>% View
+dfengine <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999, SYSTEM_TYPE_ETXT == "engine" | grepl("ENGINE", COMMENT_ETXT) | grepl("cylinder", COMMENT_ETXT) | grepl("CYLINDER", COMMENT_ETXT)) %>% mutate (SYS_FAIL = "engine"))
 
-#create new datasets to operate on
 
-dfengine <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999,  )
+#Use Union set operator to connect all into one dataframe
 
-dfbrake <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999))
+dfsysfail <- dplyr::union(dfengine, dfbrake)
 
-dfstructure <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999))
+dfsysfail <- dplyr::union(dfsysfail, dfairbag)
 
-dfengine <- data.frame(dfjames %>% select(CATEGORY_ETXT, MAKE_NAME_NM, SYSTEM_TYPE_ETXT, YEAR, COMMENT_ETXT) %>% filter (CATEGORY_ETXT %in% c("Car", "Motorcycle", "SUV") |  grepl("Truck", CATEGORY_ETXT),YEAR != -9999))
+dfsysfail <- dplyr::union(dfsysfail, dfstructure)
+View (dfsysfail)
 
+#pass into ggplot
+dfsysfail %>% ggplot(aes(x = as.character(SYS_FAIL), y = as.numeric(as.character(YEAR)), color = MAKE_NAME_NM)) + facet_grid(.~CATEGORY_ETXT) + geom_point() + theme(axis.text.x=element_text(angle=50, size=10, vjust=0.5))
 
 
 
